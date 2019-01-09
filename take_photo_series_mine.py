9e2c2dbc47e1f49e222cd4d5246a02e37ae80a27
afonit <afonit@gmail.com>
2019-01-09 15:12:36 -0500

    added some surveilance scripts

--- take_photo_series_mine.py
+++ take_photo_series_mine.py
@@ -7,6 +7,6 @@ cur_wd = os.getcwd()
 
 camera = picamera.PiCamera()
 
-for i in range(459):    
-    camera.capture(cur_wd + '/holder/current_series_image%03d.jpg' % i)
-    sleep(2)
+for i in range(9059):    
+    camera.capture(cur_wd + '/holder/current_series_image%04d.jpg' % i)
+    sleep(20)
