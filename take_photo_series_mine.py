c5e6be3fc401e81106a0c98f1480102de5bfaa89
afonit <afonit@gmail.com>
2019-01-14 16:49:16 -0500

    simplified some things

--- take_photo_series_mine.py
+++ take_photo_series_mine.py
@@ -7,6 +7,6 @@ cur_wd = os.getcwd()
 
 camera = picamera.PiCamera()
 
-for i in range(9059):    
-    camera.capture(cur_wd + '/holder/current_series_image%04d.jpg' % i)
-    sleep(20)
+for i in range(99059):    
+    camera.capture(cur_wd + '/holder/current_series_image%05d.jpg' % i)
+    sleep(2)
